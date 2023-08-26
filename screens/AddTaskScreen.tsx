3f5ba7e2f3074433b785de15b95aff8777eb8aa9
Viktoria Mesheryakova <142659686+vika-mesheryakova99@users.noreply.github.com>
2023-08-26 20:00:29 +0500

    added 'save' button on AddTask screen

--- screens/AddTaskScreen.tsx
+++ screens/AddTaskScreen.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import { View, TextInput, StyleSheet } from 'react-native';
+import { View, TextInput, StyleSheet, Text } from 'react-native';
+import CustomButton from '../components/CustomButton';
 
 // functional component for screen
 const AddTaskScreen = ({ navigation }) => {
 
+  // button tap handler
+  const handleButtonPress = () => {
+    //TODO: add save to db/store
+    navigation.navigate('Task List');
+  }
+
   // render function
   return (
     <View style={styles.container}>
-      {/* <TextInput style={{backgroundColor: 'white', width: 200, height: 50}}></TextInput> */}
+      <TextInput
+        style={styles.input}
+        placeholder="Enter task title"
+        placeholderTextColor="#999999"
+        keyboardType="default"
+      />
+      <CustomButton text='Save' onPress={handleButtonPress} />
     </View>
   );
 };
@@ -17,6 +30,18 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     backgroundColor: 'pink',
+  },
+  input: {
+    height: 50,
+    width: 400,
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 16,
+    paddingLeft: 10,
+    fontSize: 24,
+    color: '#000000',
+    backgroundColor: 'white',
+    marginTop: 50,
   },
 });
 
