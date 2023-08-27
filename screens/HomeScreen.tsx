83d5f334aac836a60eb1c57b42b7f2fa6a6bbbef
Viktoria Mesheryakova <142659686+vika-mesheryakova99@users.noreply.github.com>
2023-08-27 16:31:29 +0500

    display Tasks saved in async storage

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,18 +1,35 @@
-import React, {useEffect} from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, View, Text, FlatList } from 'react-native';
 import CustomButton from '../components/CustomButton';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // functional component for screen
 const HomeScreen = ({ navigation }) => {
 
-  //TODO: add useEffect() hook
+  const [tasks, setTasks] = useState([]);
 
-  // read from AsyncStorage
-  //const items = await AsyncStorage.getAllKeys();
-  //console.log(items);
+  useEffect(() => {
+    console.log('<<< Home screen before loading >>>');
+    fetchDataFromAsyncStorage();
+  }, []);
+
+  // read from AsyncStorage db
+  const fetchDataFromAsyncStorage = async () => {
+    try {
+      const storedData = await AsyncStorage.getAllKeys();
+      if (storedData !== null) {
+        setTasks(storedData);
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
 
+  // **************************************
+  // *********** event handlers ***********
+  // **************************************
+  
   // button tap handler
   const handleButtonPress = async () => {
 
@@ -23,6 +40,11 @@ const HomeScreen = ({ navigation }) => {
   // render function
   return (
     <View style={styles.container}>
+      <FlatList
+        data={tasks}
+        renderItem={({ item }) => <Text style={styles.task}>{item}</Text>}
+        keyExtractor={(item, index) => index.toString()}
+      />
       <CustomButton text='Add Task' onPress={handleButtonPress} />
     </View>
   );
@@ -35,6 +57,10 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     backgroundColor: 'pink',
   },
+  task: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  }
 });
 
 export default HomeScreen;
